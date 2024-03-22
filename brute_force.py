7f19b5678e02f445639dda9bc4b5f765dced92d6
osc010524 <osc010524@gmail.com>
2024-03-22 21:30:22 +0900

    완료

--- brute_force.py
+++ brute_force.py
@@ -1,14 +1,19 @@
 import itertools
 from tqdm import tqdm
 import shadowhash.__main__ as shadowhash
+import string
 
 special_char = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', '\\', '(', ')', '{', '}',
                 '[', ']', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
-upper_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-              'V', 'W', 'X', 'Y', 'Z']
-lower_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-              'v', 'w', 'x', 'y', 'z']
-number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# upper_char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+#               'V', 'W', 'X', 'Y', 'Z']
+# lower_char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+#               'v', 'w', 'x', 'y', 'z']
+# number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+upper_char = list(string.ascii_uppercase)
+lower_char = list(string.ascii_lowercase)
+number = list(string.digits)
 
 
 def brute_force(min_length: int = 1, max_length: int = 16, required_chars: str = "",cyphertext:str="",crypto_type:int=0,crypto_salt:str=""):
